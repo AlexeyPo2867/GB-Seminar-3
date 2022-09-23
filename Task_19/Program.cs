9098a90ba95266aa6a1e001cea457ec4ccccee47
alexey <shaman1801@yandex.ru>
2022-09-23 08:26:29 +0300

    Task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Полиандром или нет
+
+Console.WriteLine("Введите пятизначное число -> ");
+string str = Console.ReadLine();
+
+if ((str.Length > 5) || (str.Length < 5))
+{
+    Console.WriteLine("Число должно быть ПЯТИЗНАЧНЫМ !!!");
+    return;
+}
+
+char[] ch = str.ToCharArray();
+
+if ((ch[0] == ch[4]) && (ch[1]) == ch[3])
+    Console.WriteLine($"{str} -> Полиандром");
+else
+    Console.WriteLine($"{str} -> Не Полиандром");
